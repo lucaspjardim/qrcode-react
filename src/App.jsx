d28f70684439c873c1635225b5c53620445674b0
Lucas Jardim <lucas.pasqualijardim59@gmail.com.br>
2024-04-30 17:13:31 -0300

    Commit

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import QRCode from 'react-qr-code';
+import QRCodeLink from 'qrcode';
+import './App.css';
+
+function App() {
+  const [link, setLink] = useState('')
+  const [qrcodeLink, setQrcodeLink] = useState('')
+
+
+  function handleGenerate(link_url){
+    QRCodeLink.toDataURL(link_url, {
+      width: 600,
+      margin: 3,
+    }, function (err, url){
+      setQrcodeLink(url)
+    })
+  }
+
+  function handleQrcode(e){
+    setLink(e.target.value);
+    handleGenerate(e.target.value)
+  }
+
+  return (
+    <div className='container'>
+
+      <QRCode
+        value={link}
+        />
+
+      <input className='input' placeholder='Digite aqui seu link...' value={link} onChange={(e) => handleQrcode(e)}/>
+
+      <a href={qrcodeLink} download={`qrcode.png`} >Baixar QR Code</a>
+    </div>
+  );
+}
+
+export default App
